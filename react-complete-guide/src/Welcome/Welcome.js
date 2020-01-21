8734bcaa42702f1cc96ff69d92440300650fb7b5
williamhsa <whsantonio@gmail.com>
2020-01-20 22:12:01 -0200

    update

--- react-complete-guide/src/Welcome/Welcome.js
+++ react-complete-guide/src/Welcome/Welcome.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Welcome = (props) => {
+
+  const [count, setCount] = useState(0);
+
+  // Similar a componentDidMount e componentDidUpdate:
+  useEffect(() => {
+    console.log('component iniciou e falou')
+    // Atualiza o título do documento utilizando a API do navegador
+    document.title = `You clicked ${count} times`;
+
+    console.log('depois do click')
+
+  });
+
   return (
     <div className="Welcome">
       <h3> Welcome, {props.name}!  </h3>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   )
 }
