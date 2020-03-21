49f2ef9bce6965383e5193bdbf5c7b1167101a44
williamhsa <william.antonio@zup.com.br>
2020-03-20 23:27:50 -0300

    [4] cockpit remove class

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import classes from './Cockpit.css';
+// import classes from './Cockpit.css';
 
 const Cockpit = (props) => {
   const assignedClasses = [];
   let btnClass = '';
   
-  if (props.showPersons) btnClass = classes.Red;
-  
-  if (props.persons.length <= 2) {
-    assignedClasses.push(classes.red); // classes = ['red']
-  }
-  if (props.persons.length <= 1) {
-    assignedClasses.push(classes.bold);
-  }
+  // if (props.showPersons) btnClass = classes.Red;
+  //
+  // if (props.persons.length <= 2) {
+  //   assignedClasses.push(classes.red); // classes = ['red']
+  // }
+  // if (props.persons.length <= 1) {
+  //   assignedClasses.push(classes.bold);
+  // }
 
   return (
-    <div className={classes.Cockpit}>
+    // <div className={classes.Cockpit}>
+    <div>
       <h1>{props.title}</h1>
       <button
         className={btnClass}
