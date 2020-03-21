57a5b6a50da95c9530473fa52043219ccdbb867c
williamhsa <william.antonio@zup.com.br>
2020-03-20 23:19:04 -0300

    commit 3

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -94,6 +94,7 @@ class App extends Component {
   render() {
     console.log('1 commit');
     console.log('2 commit');
+    console.log('3 commit');
     console.log('inside render', this.state)
 
     const name = 'William'
